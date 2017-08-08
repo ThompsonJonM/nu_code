4e2dc01953ec41fbba149f3fa4aee65fbfd1370f
thompson.jonm <thompson.jonm@gmail.com>
2017-08-07 19:52:33 -0500

    express calc done

--- 20170807-expresscalc/expressCalculator.js
+++ 20170807-expresscalc/expressCalculator.js
@@ -4,10 +4,23 @@ var express = require("express");
 // Create express app instance.
 var app = express();
 
+
+// shipit - take two names and combine them together
+// bonus - remove the first letter off of the last name
+
+app.get("/:shipit/:nameOne/:nameTwo/:nameThree", function(req, res) {
+  var shipit = req.params.shipit;
+  var nameOne = req.params.nameOne;
+  var nameTwo = req.params.nameTwo;
+  var nameThree = req.params.nameThree;
+
+  res.send(req.params.nameOne + req.params.nameTwo + req.params.nameThree);
+});
+
 // Routes
 // What routes do you need to have? Which ones are optional?
 // TODO Add your routes here
-app.get("/:oparation/:numOne/:numTwo", function(req, res) {
+app.get("/:operation/:numOne/:numTwo", function(req, res) {
   var operation = req.params.operation;
 
   // + prior to req auto-changes the string input to a number
@@ -26,15 +39,27 @@ app.get("/:oparation/:numOne/:numTwo", function(req, res) {
     // BONUS - How could you use * + etc. inside the app.get()?
     case "add":
       // Add your logic here. Pun intended.
+
+      result = numOne + numTwo;
+
       break;
     case "subtract":
       // Subtract logic
+
+      result = numOne - numTwo;
+
       break;
     case "multiply":
       // Multiply
+
+      result = numOne * numTwo;
+
       break;
     case "divide":
       // Divide
+
+      result = numOne / numTwo;
+
       break;
     default:
       // Handle anything that isn't specified

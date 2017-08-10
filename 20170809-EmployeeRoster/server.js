aece3558652489097fc61f5e869af25c4315fb42
thompson.jonm <thompson.jonm@gmail.com>
2017-08-09 21:38:17 -0500

    stranger things

--- 20170809-EmployeeRoster/server.js
+++ 20170809-EmployeeRoster/server.js
@@ -1,13 +1,58 @@
 //Dependencies
 var express = require('express');
 var exphbs = require('express-handlebars');
+var mysql = require('mysql');
 
 var PORT = 3000;
 
 var app = express();
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    database: 'employeesdb',
+    user: 'root',
+    password: 'root'
+});
+
+connection.connect(function(err, res) {
+    if (err) throw err;
+    console.log(`${connection.threadId}`);
+});
+
+// var employees = [
+//     {
+//         name: 'Steve',
+//         department: 'Waste Management',
+//     },
+//     {
+//         name: 'Gary',
+//         department: 'Cucumber Island'
+//     },
+//     {
+//         name: 'Linda from HR',
+//         department: 'HR'
+//     }
+// ]
+
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {});
+});
+
+app.get('/employees', function(req, res) {
+
+    connection.query('select * from employee', function(err, data) {
+        if (err) throw err;
+
+        res.render('employees', {
+            title: 'Fired Employees',
+            employeeList: data
+
+        });
+    });
 });
 
 app.listen(PORT, function() {
